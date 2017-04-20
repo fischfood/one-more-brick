e49245bd2041c4aaf0257b64fa59b9b21f994163
Brian Fischer <brian@fischfooddesigns.com>
2017-04-20 00:14:29 -0400

    Gameplay and debug/button sidebars

---
+++ js/gameplay.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	var start = 1;
+	
+	$('.generate').click( function() {
+		$('.game').prepend('<div class="row"><div class="block"></div><div class="block"></div><div class="block"></div></div>');
+	});
+
+});
